8a89f0d1a61b942d80722bce3918af7ea8c89ddd
marshi <mukai_masaki@cyberagent.co.jp>
2017-01-09 17:10:39 +0900

    1.8対応

--- app/src/main/java/android/marshi/wave/MainActivity.java
+++ app/src/main/java/android/marshi/wave/MainActivity.java
@@ -3,6 +3,8 @@ package android.marshi.wave;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.annimon.stream.Stream;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
